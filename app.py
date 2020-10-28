0d2abb551f9ab1f144567c9a3a3b991b40d20cbd
Abhijit Jathar <abhijit.jathar@ebosgroup.com>
2020-10-28 20:03:42 +1100

    removing flask cors again

--- app.py
+++ app.py
@@ -5,24 +5,24 @@ sys.dont_write_bytecode = True
 
 import json
 from flask import Flask, request
-from flask_cors import CORS, cross_origin
+# from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 
-cors = CORS(app)
-app.config["CORS_HEADERS"] = "Content-Type"
+# cors = CORS(app)
+# app.config["CORS_HEADERS"] = "Content-Type"
 
 from constants import *
 from sources.source import source
 
 @app.route('/api/')
-@cross_origin()
+# @cross_origin()
 def getsources():
     return json.dumps(misc.Sources)
 
 @app.route('/api/<src>')
 @app.route('/api/<src>/<int:n>')
-@cross_origin()
+# @cross_origin()
 def next(src, n:int = 0):
     q = request.args.get("q") if "q" in request.args.keys() else request.query_string.decode()
     return json.dumps(source(src).GetNext(n,q))
